f7174b315ebb5285e1690c8cf4749e7fbff55d69
derton1996 <zulusvlad18@gmail.com>
2021-07-26 02:31:49 +0700

    KIN-8 добавил меню снизу

--- src/main/java/org/kinosearch/config/AppConfig.java
+++ src/main/java/org/kinosearch/config/AppConfig.java
@@ -64,6 +64,10 @@ public class AppConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/css/**")
                 .addResourceLocations("/WEB-INF/css/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("/WEB-INF/js/");
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("/WEB-INF/img/");
     }
 
 }
